fe86779b6ed5c0b795194a72ca114991875729ca
Farel Arlish Orlando <mobillamborghini9999@gmail.com>
2026-06-24 22:33:14 +0700

    chore: add config middleware

--- backend/api/server.js
+++ backend/api/server.js
@@ -10,7 +10,12 @@ const app = express();
 
 dotenv.config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://rel-stack.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
